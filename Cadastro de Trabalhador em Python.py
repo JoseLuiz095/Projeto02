04187c71d28d626b988a9e26d41b3611f0628487
José Luiz <82607322+JoseLuiz095@users.noreply.github.com>
2022-11-19 16:47:37 -0300

    Update Cadastro de Trabalhador em Python.py

--- Cadastro de Trabalhador em Python.py
+++ Cadastro de Trabalhador em Python.py
@@ -1,13 +1,39 @@
 from datetime import datetime
-dados=dict()
-dados['Nome: ']=str(input('Nome: '))
-nac=int(input('Ano de nascimento: '))
-dados['Idade: ']=datetime.now().year - nac
-dados['ctps: '] = int(input('Carteira de Trabalho (o valor 0 é porque não tem ctps): '))
-if dados['ctps: '] !=0:
+
+
+dados = dict()
+
+
+# perguntas para o dados
+dados['Nome: '] = str(input('Nome: '))
+while True:    
+    dados['Sexo: ']=str(input('sexo[M/F]')).strip().upper()[0]
+    if dados['Sexo: '] in'MF':
+        break
+    print('Erro sexo invalido!')
+nac = int(input('Ano de nascimento: '))
+dados['Idade: '] = datetime.now().year - nac
+dados['ctps: '] = int(input('Carteira de Trabalho(Pis,caso não houver digite 0): '))
+
+# Area de calculo do ctps/inss
+if dados['ctps: '] != 0:
     dados['Contratação: '] = int(input('Ano de contratação: '))
-    dados['Salário: ']=float(input('Salário: R$'))
-    dados['Aposentadoria com a idade: '] = dados['Idade: ']+((dados['Contratação: '] +35)-datetime.now().year)
+    dados['Salário: R$'] = float(input('Salário: R$'))
+    if dados['Salário: R$'] <= 1212.00:
+        Desconto = dados['Salário: R$']*0.075
+    elif dados['Salário: R$'] >= 1212.00 and dados['Salário: R$']<=2427.35:
+        Desconto = dados['Salário: R$']*0.09
+    elif dados['Salário: R$'] >= 2427.36 and dados['Salário: R$']<= 3641.03:
+        Desconto = dados['Salário: R$']
+    elif dados['Salário: R$'] >= 3641.03:
+        Desconto = dados['Salário: R$']*0.14
+    dados['Desconto do Inss: R$'] = Desconto
+    if dados['Sexo: ']=='M':
+        dados['Aposentadoria com a idade: '] = dados['Idade: '] + ((dados['Contratação: '] + 35)-datetime.now().year)
+    else:
+        dados['Aposentadoria com a idade: '] = dados['Idade: '] + ((dados['Contratação: '] + 30)-datetime.now().year)
 print('-='*30)
-for n,v in dados.items():
+
+# print na tela
+for n, v in dados.items():
     print(f' -> {n}{v}')
